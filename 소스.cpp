8a1a7486fbedf788027d8ded2332228d6035da39
gggoo <50660440+qlasdw@users.noreply.github.com>
2022-03-31 20:52:03 +0900

    Add files via upload

--- 소스.cpp
+++ 소스.cpp
@@ -1,13 +1,38 @@
 #include <iostream>
+#include<string>
 using namespace std;
 
-int main() {
-	for (int i = 1; i <= 100; i++)
-	{
-		cout << i << "\t";
-		if (i % 10 == 0)
-		{
-			cout << "\n";
-		}
+class Person{
+	string name;
+	int size;
+public:
+	Person(){};
+	Person(string name) { this->name = name; }
+	string getName() { return name; }
+	
+};
+
+class Family{
+	Person *p;
+	int size;
+	string f;
+public:
+	Family(string name, int size){ this->size = size; p = new Person[size]; }
+	void show(){ cout  <<  "Simpson가족은 다음과 같이 " << size << "명 입니다." << endl;
+	for (int i = 0; i < size; i++){
+		cout << p[i].getName() << "\t";
 	}
+	}
+	void setName(int size, string name){ Person a(name); p[size] = a; }
+	~Family() { delete[]p; }
+};
+
+
+int main(){
+	Family *simpson = new Family("Simpson", 3);
+	simpson->setName(0, "Mr. Simpson");
+	simpson->setName(1, "Mrs. Simpson");
+	simpson->setName(2, "Bart. Simpson");
+	simpson->show();
+	delete simpson;
 }
